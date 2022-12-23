bce993d545eff14b2b2aa64433a70ce523ff9c95
s3a6m9 <74426924+s3a6m9@users.noreply.github.com>
2022-12-23 21:55:27 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 
 WORKING_PATH = os.path.abspath(os.path.dirname(__file__))
 DEFAULT_CONFIG_PATH = os.path.join(WORKING_PATH, "config.txt")
+BOT_DATA_PATH = os.path.join(os.path.join(WORKING_PATH, "bot"), "bot.dat")
 
 
 def parse_config(config_path):
@@ -14,3 +15,22 @@ def parse_config(config_path):
             line = line.split("=")
             config_variables[line[0].strip().lower()] = line[1].strip()
     return config_variables
+
+
+def file_exists(file_path):
+    if os.path.isfile(file_path):
+        return True
+    else:
+        return False
+
+def create_file(file_path):
+    with open(file_path, "w"):
+        pass
+
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+
+def write_file(file_path, content):
+    with open(file_path, "w") as file:
+        file.write(content)
